8e5f22c6a03a01d524caf3f099337502cca50afa
Ruslan <saharcorez@gmail.com>
2026-08-25 15:52:48 +0300

    chore: настроить базовую инфраструктуру проекта

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import prisma from './prisma';
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: 'postgresql' }),
+  emailAndPassword: { enabled: true },
+    user: {
+    additionalFields: {
+      role: {
+        type: ["user", "admin", "teacher"],
+        required: false,
+        defaultValue: "user",
+        input: false,
+      },
+    },
+  },
+});
