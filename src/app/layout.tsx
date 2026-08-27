60c0a530ec9bd58541e3c47b60f79391fb104c33
Ruslan <saharcorez@gmail.com>
2026-08-27 12:56:44 +0300

    fix: исправить плагин prettier линтера

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Public_Sans, Inter } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/shared/lib/utils';
 
